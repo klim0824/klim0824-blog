58fc879bd88ee8097cfcd68b03a7befe54664aeb
klim <klim0824@live.jp>
2022-03-07 23:50:37 +0900

    add types of microsms

---
+++ src/types/api.ts
@@ -0,0 +1,62 @@
+type ListContents<T> = {
+  contents: T[]
+  totalCount: number
+  offset: number
+  limit: number
+}
+
+type Common<T> = {
+  id?: string
+  createdAt?: string
+  updatedAt?: string
+  publishedAt?: string
+  revisedAt?: string
+} & T
+
+type image = {
+  url: string
+  height: number
+  width: number
+}
+
+export type ArticlesList = ListContents<Article>
+
+type Article = Common<{
+  title?: string
+  description?: string
+  content?: string
+  category?: Category
+  tags?: Tag[]
+  mainvisual?: image
+}>
+
+export type TagsList = ListContents<Tag>
+
+export type Tag = Common<{
+  tag?: string
+  icon?: image
+}>
+
+export type CategoriesList = ListContents<Category>
+
+export type Category = Common<{
+  name?: string
+}>
+
+export type AccountsList = ListContents<Account>
+
+export type Account = Common<{
+  name?: string
+  url?: string
+  image?: image
+  color?: string
+}>
+
+export type Meta = Common<{
+  title?: string
+  url?: string
+  description?: string
+  author?: string
+  'author-id'?: string
+  'author-image'?: image
+}>
