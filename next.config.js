ae6fb44a4f86b8ad8e4851d7c10ad8f231faf0af
klim <klim0824@live.jp>
2022-03-09 15:34:59 +0900

    update nextjs settings

--- next.config.js
+++ next.config.js
@@ -9,6 +9,53 @@ const nextConfig = {
 
     return config
   },
+  images: {
+    domains: ['images.microcms-assets.io'],
+    disableStaticImages: true,
+  },
+  reactStrictMode: true,
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: '/(.*?)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ]
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/articles',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/categories',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/tags',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
