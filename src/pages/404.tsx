f4ecb70d4db491e518c72fa2d2ae17405ecb8696
klim <klim0824@live.jp>
2022-03-18 02:43:37 +0900

    add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+import { InferGetStaticPropsType, NextPage } from 'next'
+import { Box, Heading, Link, Text } from '@chakra-ui/react'
+import { NextSeo } from 'next-seo'
+
+import Layout from 'components/Layout'
+import { fetchAccounts, fetchMeta } from 'libs/fetchApi'
+
+type PageProps = InferGetStaticPropsType<typeof getStaticProps>
+
+const NotFound: NextPage<PageProps> = (props) => {
+  const { accounts, meta } = props
+
+  return (
+    <>
+      <NextSeo
+        noindex={true}
+        nofollow={true}
+        title={'404 Page Not Found | ' + meta.title}
+      />
+      <Layout accounts={accounts} meta={meta}>
+        <Box
+          maxW="4xl"
+          m="auto"
+          p={4}
+          border="1px"
+          borderColor="gray.100"
+          borderRadius="md"
+          boxShadow="sm"
+          bg="white"
+        >
+          <Heading as="h1" pb="4" textAlign="center">
+            404 Page Not Found.
+          </Heading>
+          <Text textAlign="center">
+            <Link href="/">Go to Homepage</Link>
+          </Text>
+        </Box>
+      </Layout>
+    </>
+  )
+}
+export const getStaticProps = async () => {
+  const accounts = await fetchAccounts()
+  const meta = await fetchMeta()
+
+  return {
+    props: {
+      accounts,
+      meta,
+    },
+  }
+}
+
+export default NotFound
